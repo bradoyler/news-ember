dea1e594028bca15ef3c5e35ce0180fa3e7fd7d5
Brad Oyler <bradoyler@gmail.com>
2014-03-26 08:58:47 -0700

    add route files

---
+++ app/routes/health.js
@@ -0,0 +1,12 @@
+import getJSON from 'appkit/utils/get_json';
+
+export default Ember.Route.extend({
+
+	renderTemplate: function() {
+      this.render('section');
+    },
+
+	model: function() {
+		return getJSON('/1.0/entriesbysection?section=health');
+	}
+});

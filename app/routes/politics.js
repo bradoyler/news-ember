405815ddb737f88744a14b6105161c67ccb80c4e
Brad Oyler <bradoyler@gmail.com>
2014-03-27 10:05:42 -0700

    cleanup & article api-stub

--- app/routes/politics.js
+++ app/routes/politics.js
@@ -2,9 +2,7 @@ import getJSON from 'appkit/utils/get_json';
 
 export default Ember.Route.extend({
 
-	renderTemplate: function() {
-      this.render('section');
-    },
+    templateName:'section',
 
 	model: function() {
 		return getJSON('/1.0/entriesbysection?section=politics');

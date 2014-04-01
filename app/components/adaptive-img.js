9ede87e23a52ca9cfe664022f9fb62a75ed1cc81
Brad Oyler <bradoyler@gmail.com>
2014-04-01 12:38:09 -0400

    support other types and png images

--- app/components/adaptive-img.js
+++ app/components/adaptive-img.js
@@ -7,7 +7,9 @@ default Ember.Component.extend({
 		var width= this.get('width') || '520';
 		var height= this.get('height') || '240';
 		var imgurl = this.get('imgurl') ||'';
-		imgurl = imgurl.replace('/i/', '/j/').replace('.jpg', '.nbcnews-fp-'+width+'-'+ height +'.jpg');
+
+		imgurl = imgurl.replace('/i/', '/j/').replace('.jpg', '.nbcnews-fp-'+width+'-'+ height +'.jpg')
+		.replace('.png', '.nbcnews-fp-'+width+'-'+ height +'.png');
 		return imgurl;
 	}.property('name')
 });

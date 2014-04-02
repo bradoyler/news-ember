2bd55df6855e1fb3caa857e4b7c8759d0ab9eac1
Brad Oyler <bradoyler@gmail.com>
2014-04-01 23:57:16 -0400

    route cleanup and unbound optimization

--- app/routes/section.js
+++ app/routes/section.js
@@ -2,10 +2,7 @@ import getJSON from 'appkit/utils/get_json';
 
 export default Ember.Route.extend({
 
-	templateName:'section',
-
 	model: function(params) {
-		return getJSON(window._apiurl +'/entriesbysection?section=news');
+		return getJSON(window._apiurl +'/entriesbysection?section='+ params.id );
 	}
-
 });

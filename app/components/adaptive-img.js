1266dd8b08286beed963d79c3e93a330209cd62f
Brad Oyler <bradoyler@gmail.com>
2014-04-02 12:59:31 -0400

    img BP

--- app/components/adaptive-img.js
+++ app/components/adaptive-img.js
@@ -4,9 +4,14 @@ default Ember.Component.extend({
 	classNames: ['panel-img-lede'],
 	attributeBindings: ['src', 'alt'],
 	src: function() {
+		var $win = Ember.$(window);
 		var width= this.get('width') || '520';
 		var height= this.get('height') || '240';
 		var imgurl = this.get('imgurl') ||'';
+
+		if($win.width() < 614) {
+			width= 400; height=200;
+		}
 
 		imgurl = imgurl.toLowerCase().replace('/i/', '/j/').replace('.jpg', '.nbcnews-fp-'+width+'-'+ height +'.jpg')
 		.replace('.png', '.nbcnews-fp-'+width+'-'+ height +'.png');
